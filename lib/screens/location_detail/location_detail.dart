8f3f546e199a0a8da3516960cbbcb4115548ca7e
Félix Boquet <felix.boquet@in-tact.fr>
2021-04-14 10:20:04 +0200

    First commit

---
+++ lib/screens/location_detail/location_detail.dart
@@ -0,0 +1,35 @@
+// screens/location_detail.location_detail.dart
+
+import 'package:flutter/material.dart';
+import 'image_banner.dart';
+import 'package:flutter_application_1/screens/location_detail/text_section.dart';
+import '../../models/location.dart';
+
+class LocationDetail extends StatelessWidget {
+  final int _locationId;
+
+  LocationDetail(this._locationId);
+
+  @override
+  Widget build(BuildContext context) {
+    final location = Location.fetchByID(_locationId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(location.name),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ImageBanner(location.imagePath),
+          ]..addAll(textSections(location))),
+    );
+  }
+
+  List<Widget> textSections(Location location) {
+    return location.facts
+        .map((fact) => TextSection(fact.title, fact.text))
+        .toList();
+  }
+}
